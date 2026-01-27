1e7d782cb93121c9ed2673f761d1ce4024875e38
Lukáš Bujdoš <56771012+bujdoluk@users.noreply.github.com>
2026-01-27 10:55:26 +0000

    feat: added fetchJoke method

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import Home from "./Home.tsx";
 import "./styles.css";
 import Favourites from "./Favourites.tsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
-    <div className="App">
-      <Home />
-      <Favourites />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favourites" element={<Favourites />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,10 +1,49 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 
 export default function Home() {
+  const [joke, setJoke] = useState<Joke>("");
+  const [loading, setLoading] = useState<boolean>(false);
+  const timeoutRef = useRef<number | null>(null);
+
+  interface Joke {
+    text: string;
+  }
+
+  const fetchJoke = async (): Promise<void> => {
+    try {
+      setLoading(true);
+      const response: Response = await fetch(
+        "https://api.chucknorris.io/jokes/random"
+      );
+      const data = await response.json();
+      setJoke(data.value);
+    } catch (error: unknown) {
+      console.log("Could not fetch a joke", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleClick = (): void => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+
+    timeoutRef.curent = window.setTimeout(async (): Promise<void> => {
+      await fetchJoke();
+    }, 500);
+  };
+
   return (
-    <div>
-      <button>GET joke</button>
+    <div className="homePage">
+      <button onClick={handleClick} disabled={loading}>
+        {loading ? "Loading..." : "GET one joke"}
+      </button>
       <button>Click me</button>
+
+      <div className="grid">
+        <div>{joke}</div>
+      </div>
     </div>
   );
 }
